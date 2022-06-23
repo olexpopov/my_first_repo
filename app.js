cf5736e3df997a32db2566f0102b3798b1d669fc
Alex <aleksandrpopov3011@gmail.com>
2022-06-23 18:15:08 +0300

    video 16

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
-
-const getNotes = require ('./notes.js')
 const chalk = require ('chalk')
+const { strictCommands } = require('yargs')
+const yargs = require ('yargs')
+const getNotes = require ('./notes.js')
 
-const command = process.argv[2]
-
-console.log(process.argv)
-
-if (command == 'add')
-{
-    console.log('Adding note')
+yargs.version('1.1.0')
+yargs.command({
+command: 'add',
+describe:'add a new note',
+handler:function(){
+    console.log('adding a new note')
 }
-else if(command == 'remove ')
-{
-    console.log('remove note')
-}
+})
+
+yargs.command({
+    command: 'remove',
+    describe:'remove note',
+    handler:function(){
+        console.log('removing notes')
+    }
+})
+    yargs.command({
+        command: 'read',
+        describe:'read note',
+        handler:function(){
+            console.log('reading note')
+        }
+        })
+
+
+    yargs.command({
+        command: 'list',
+        describe:'list note',
+        handler:function(){
+            console.log('listing notes')
+        }
+        })
+
+
+
+console.log(yargs.argv)
+
+// if (command == 'add')
+// {
+//     console.log('Adding note')
+// }
+// else if(command == 'remove ')
+// {
+//     console.log('remove note')
+// }
 // const x = getNotes()
 // const greenMsg = chalk.green.bold('Success!')
 // console.log(x)
